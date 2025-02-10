d366d2bd2f6fd8e6679561e862f5ac34f49ab034
Francisco Laines <pacosebastian2002@hotmail.com>
2025-02-10 11:02:34 -0500

    10 02 2025 funcion string.h

--- cadena/1946_LainesFrancisco_Cadena_0.1.cpp
+++ cadena/1946_LainesFrancisco_Cadena_0.1.cpp
@@ -22,8 +22,7 @@ int contarPalabras(char cad[]) // validacion por si hay espacios seguidos//
 {
     int palabras = 0, i;
     bool palabra = false;
-    /*RECUENTO DE PALABRAS SEGUN EL NUMERO DE ESPACIOS
-    for (i = 0; cad[i] != '\0'; i++)
+    /*for (i = 0; cad[i] != '\0'; i++)
     {
         if ((cad[i] == ' ' && cad[i + 1] != ' ' && cad[i + 1] != '\0'))
         {
@@ -47,7 +46,6 @@ int contarPalabras(char cad[]) // validacion por si hay espacios seguidos//
     }
     return palabras;*/
 
-    //RECEUNTO DE PALABRAS SEGUNEL VALOR BOOLEANO DE PALABRA//
     for (i = 0; cad[i] != '\0'; i++)
     {
         if (cad[i] != ' ' && !palabra)
